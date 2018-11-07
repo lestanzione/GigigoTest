81017233a3546020ec59006f7f4a625833d91c10
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-11-07 21:24:42 -0200

    Add HomeFragment raw layout

---
+++ app/src/main/java/br/com/stanzione/gigigotest/home/HomeFragment.java
@@ -0,0 +1,24 @@
+package br.com.stanzione.gigigotest.home;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.stanzione.gigigotest.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
