f71f8e68f39c1436da431c429a438f03881eaf9d
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-11-08 10:40:56 -0200

    Create ProductDetail layout

---
+++ app/src/main/java/br/com/stanzione/gigigotest/productdetail/ProductDetailActivity.java
@@ -0,0 +1,15 @@
+package br.com.stanzione.gigigotest.productdetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.stanzione.gigigotest.R;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
