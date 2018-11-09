6fac61162032415db432ab30f2bb4b2c181649eb
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-11-09 09:06:45 -0200

    Create OrdersFragment layout

---
+++ app/src/main/java/br/com/stanzione/gigigotest/orders/OrdersFragment.java
@@ -0,0 +1,23 @@
+package br.com.stanzione.gigigotest.orders;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.stanzione.gigigotest.R;
+
+public class OrdersFragment extends Fragment {
+
+    public OrdersFragment() {}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_orders, container, false);
+    }
+
+}
